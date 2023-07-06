743f0ce32b880c58534329b25fd70447abc16418
Madina Dzhetegenova <my@Madinas-MacBook-Air.local>
2023-07-06 10:44:57 -0700

    header commponent

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,14 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-
+import { Header } from "./components/header/Header";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <p>Images Gallery</p>
-    </>
+    <div>
+      <Header title="Images Gallery" />
+    </div>
   );
 }
 
