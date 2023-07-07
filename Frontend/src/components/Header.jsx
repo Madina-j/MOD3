91d8155c77efa2525202821136b22521b3d60fe8
Madina Dzhetegenova <my@Madinas-MacBook-Air.local>
2023-07-07 13:22:30 -0700

    add logo

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,6 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import { Navbar, Container } from "react-bootstrap";
+import { ReactComponent as Logo } from "../images/logo.svg";
 
 const navbarStyle = {
   backgroundColor: "lightblue",
@@ -9,7 +10,7 @@ export const Header = ({ title }) => {
   return (
     <Navbar style={navbarStyle} variant="light">
       <Container>
-        <Navbar.Brand href="/">{title}</Navbar.Brand>
+        <Logo style={{ maxWidth: "12rem", maxHeight: "2rem" }} />
       </Container>
     </Navbar>
   );
