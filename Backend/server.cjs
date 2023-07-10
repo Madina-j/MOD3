cfcb6625f0fb9e1db44a25214a8d3f963f1e35ce
Madina Dzhetegenova <my@Madinas-MacBook-Air.local>
2023-07-10 11:52:57 -0700

    add router

--- Backend/server.cjs
+++ Backend/server.cjs
@@ -1,10 +1,25 @@
 const mongoose = require("mongoose");
 const express = require("express");
-
+const dotenv = require("dotenv");
 const app = express();
+const routes = require("./routers/routes.cjs");
 
+app.use("/api", routes);
+app.get("/", (req, res) => {
+  res.send("OK");
+});
+dotenv.config();
 app.use(express.json());
 
-app.listen(3000, () => {
-  console.log(`Server Started at ${3000}`);
-});
+const start = async () => {
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.MONGO_LOGIN}:${process.env.MONGO_PASSWORD}@cluster0.xx4op9u.mongodb.net/`
+    );
+    app.listen(3000, () => {
+      console.log(`Server Started at ${3000}`);
+    });
+  } catch (e) {}
+};
+
+start();
