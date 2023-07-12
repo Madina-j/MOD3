036ccd4e5a4aaf1286ea1db120d4bfdf8af5c04f
Madina Dzhetegenova <mdzhetegenova@LABA.PSWAS.LOCAL>
2023-07-12 15:49:59 -0700

    render hosting

--- Backend/server.cjs
+++ Backend/server.cjs
@@ -5,15 +5,15 @@ const app = express();
 const routes = require("./routers/routes.cjs");
 const cors = require("cors");
 const path = require("path");
-app.get("/*", function (req, res) {
-  res.sendFile("index.html", {
-    root: path.join(__dirname, "./dist"),
-  });
-});
+// app.get("/*", function (req, res) {
+//   res.sendFile("index.html", {
+//     root: path.join(__dirname, "./dist"),
+//   });
+// });
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static("dist"));
+app.use(express.static(path.join(__dirname, "../Backend/dist")));
 
 app.use("/api", routes);
 app.get("/", (req, res) => {
